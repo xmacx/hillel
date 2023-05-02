11cdb2e210d6881ce9f04500c90e51dc4a2d2445
xmacx <hochusche@gmail.com>
2023-05-02 22:55:08 +0200

    Update hw-31.js

--- hw-31.js
+++ hw-31.js
@@ -3,46 +3,53 @@ const fetchAllProducts = async () => {
   return (await fetch("https://dummyjson.com/products")).json();
 };
 
-const productImage = product => `
-<div class="image-wrapper">
-  <img src="${ product.thumbnail }" alt="${ product.title }" class="image" />
-</div>`;
+const productImage = function() { 
+  return `
+  <div class="image-wrapper">
+    <img src="${ this.thumbnail }" alt="${ this.title }" class="image" />
+  </div>`;
+}
 
-const productTitle = product => `
-<div class="title">
-  <h4>${ product.title }</h4>
-</div>`;
+const productTitle = function() { 
+  return `
+  <div class="title">
+    <h4>${ this.title }</h4>
+  </div>`;
+}
 
-const productPrice = product => {
-  const price = product.price;
+const productPrice = function() {
+  const price = this.price;
   let discount,
       salePrice;
-  if (product.discountPercentage !== '' && product.discountPercentage !== 0) {
-    discount = product.discountPercentage;
+  if (this.discountPercentage !== '' && this.discountPercentage !== 0) {
+    discount = this.discountPercentage;
     salePrice = (price - price / 100 * discount).toFixed(2);
   }
   
-  return `<div class="price">${price} ${discount !== 0 ? ", price with discount " + salePrice : ''}</div>`;
+  return `<div class="price">${price}, ${discount !== 0 ? "price with discount " + salePrice : ''}</div>`;
 }
 
-const productDescription = product => `<div class="description">${product.description}</div>`;
+const productDescription = function() {
+  return `<div class="description">${this.description}</div>`;
+} 
 
-const productActions = () => `
-<div class="actions">
-  <button id="cart" class="button green-solid cart">Add to Cart</button>
-  <button class="button more">More Details</button>
-</div>`;
+const productActions = function() {
+  return `
+  <div class="actions">
+    <button id="cart" class="button green-solid cart">Add to Cart</button>
+    <button class="button more">More Details</button>
+  </div>`;
+}
 
-const productItem = (product) => {
-  console.log(product);
+const productItem = function() {
   return `
   <section class="product-item">
-    ${productImage(product)}
+    ${productImage.call(this)}
     <div class="content-wrapper">        
-      ${productTitle(product)}
-      ${productPrice(product)}
-      ${productDescription(product)}
-      ${productActions()}                  
+      ${productTitle.call(this)}
+      ${productPrice.call(this)}
+      ${productDescription.call(this)}
+      ${productActions.call(this)}                  
     </div>
   </section>`;
 }
@@ -53,7 +60,7 @@ async function getAllProducts() {
 
   const productsTamplate = `
    <article class="products">
-        ${prouducts.map((product) => productItem(product)).join("")}
+        ${prouducts.map((product) => productItem.call(product)).join("")}
    </article>
   `;
 
