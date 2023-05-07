e3746cf8572c52847d8cabcdd5ec9f55baa88959
xmacx <hochusche@gmail.com>
2023-05-07 16:06:03 +0200

    Create hw-32.js

---
+++ hw-32.js
@@ -0,0 +1,56 @@
+const filterProducts = (...args) => {
+  const [products, params] = args;
+  const filterParam = params[params.length - 1];  
+  const filterParamKey = Object.keys(filterParam)[0]
+
+  if (Object.values(filterParam)[0]) {
+    const filteredProducts = products.filter((product) => {
+        return product[filterParamKey] == filterParam[filterParamKey];
+    });
+    
+    return filteredProducts;
+  } else {
+    return products;
+  }
+
+}
+
+async function getAllProducts(...params) {
+  const response = await fetchAllProducts();
+  const prouducts = !params.length ? response.products : filterProducts(response.products, params);
+
+  if (prouducts.length) {
+    const productsTamplate = `
+       <article class="products">
+          ${prouducts
+            .sort(function(a, b){
+              const ratingA = a.rating;
+              const ratingB = b.rating;
+              return ratingB - ratingA;
+            })
+            .map((product) => {
+              const { brand, category } = product;
+              const classesList = [
+                brand.split(" ").join("-").toLowerCase(),
+                category.toLowerCase(),
+              ];
+  
+              return getProductItem(product, classesList);
+            })            
+            .join("")}
+       </article>
+      `;
+  
+    document.getElementById("app").innerHTML = productsTamplate;    
+  } else {
+    document.getElementById("app").innerHTML = 'There are no products by your search request';    
+  }
+}
+
+const getSearchParam = (param) => {
+  const value = prompt(`Enter the valuer of "${param}" searching parameter`);
+  return {[param]: value};
+}
+
+// getAllProducts();
+getAllProducts(getSearchParam('price'), getSearchParam('brand'));
