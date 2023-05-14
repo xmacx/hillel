316764cd52113f4896d386a198a25b04ad0cdf94
xmacx <hochusche@gmail.com>
2023-05-14 20:25:55 +0200

    Create hw-34.js

---
+++ hw-34.js
@@ -0,0 +1,22 @@
+function* getPartsOfWord (string) {
+  let i = 0,
+      value;
+
+  while (i <= string.length) {
+    i++;
+
+    if (i > string.length) {
+      return string.slice(0, i);      
+    } else {
+      value = value ? yield string.slice(0, i).concat(value) : yield string.slice(0, i);
+    }
+  }
+}
+
+const iterator = getPartsOfWord("hello");
+console.log(iterator.next()); // h
+console.log(iterator.next()); // he
+console.log(iterator.next('!')); // hel!
+console.log(iterator.next()); // hell
+console.log(iterator.next('@')); // hello@
+console.log(iterator.next()); // hello
