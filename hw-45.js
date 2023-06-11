26227bccd2245e6529d8b2a9789f985faca9e11b
xmacx <hochusche@gmail.com>
2023-06-11 10:36:43 +0200

    Add HW_45

---
+++ hw-45.js
@@ -0,0 +1,16 @@
+// HW 45. Dom
+
+// Есть 3 input. Выводить в textarea содержимое всех полей ввода через запятую. Использовать setInterval.
+// Кожні N-sec перевіряти інпути (input.value) і якщо є зміни, то додавати в textarea
+
+const inputs = [ ...document.getElementsByTagName('input') ];
+const textarea = document.querySelector('textarea');
+
+setInterval(() => {
+  textarea.value = '';
+  textarea.value = inputs.reduce((res, input, index) => {
+    if (input.value !== '') {
+      return  res + `Input ${index + 1} = ${input.value}\n`;
+    }
+  }, '');
+}, 1000)
